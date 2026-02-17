3b5d69e9c24c02ee05da75d8f1d0b7270c787783
sushma655 <sushmakum.655@gmail.com>
2026-02-18 01:15:39 +0530

    Time: 7 ms (14.93%), Space: 12.1 MB (5.07%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,11 @@
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string s = to_string(x);
+        string rev = string(s.rbegin(), s.rend());
+        return s == rev;
+    }
+};
